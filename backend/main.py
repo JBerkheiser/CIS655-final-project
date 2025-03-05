9e027b8e4eb783beaa113c36dacc400949ff53b5
Joe Berkheiser <berkheij@mail.gvsu.edu>
2025-03-05 14:01:49 -0500

    added to the backend.  Set it up so it is like assignment 8.  Compute engine to be added

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import random
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/get-problem")
+def getMathProblem():
+    operatorList = ['+', '-', '*', '/']
+    firstNumber = random.randint(1, 100)
+    secondNumber = random.randint(1, 100)
+    operator = random.choice(operatorList)
+
+    retString = jsonify({"firstNumber": firstNumber, "secondNumber": secondNumber, "operator": operator})
+    return retString
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
---
+++ frontend/script.js
@@ -0,0 +1,58 @@
+firstNumber = 0;
+secondNumber = 0;
+operator = "+";
+answer = 0;
+score = 0;
+
+function start()
+{
+        getMathProblem();
+        document.getElementById("Start").innerText="Submit";
+        document.getElementById("Start").onclick=checkAnswer;
+}
+
+function getMathProblem()
+{
+        fetch("https://cis655-assignment8-synthesis.ue.r.appspot.com/get-problem")
+        .then(response => response.json())
+        .then(data =>
+        {
+                firstNumber = data.firstNumber;
+                secondNumber = data.secondNumber;
+                operator = data.operator;
+                document.getElementById("Problem").innerText = firstNumber + " " + operator + " " + secondNumber + " = ?";
+        })
+        .catch(error => console.error("Error fetching math problem:", error));
+}
+
+function solveProblem()
+{
+        switch(operator)
+        {
+                case "-":
+                        answer = (parseInt(firstNumber) - parseInt(secondNumber)).toString();
+                        break;
+                case "*":
+                        answer = (parseInt(firstNumber) * parseInt(secondNumber)).toString();
+                        break;
+                case "/":
+                        answer = (Math.floor(parseInt(firstNumber) / parseInt(secondNumber))).toString();
+                        break;
+                default:
+                        answer = (parseInt(firstNumber) + parseInt(secondNumber)).toString();
+
+        }
+}
+
+function checkAnswer()
+{
+        userAnswer = document.getElementById("answer").value;
+        solveProblem();
+        console.log("answer: " + answer);
+        if(userAnswer == answer)
+        {
+                score += 1;
+                document.getElementById("Score").innerText = "Score: " + score;
+        }
+        getMathProblem();
+}
