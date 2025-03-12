1ca9c82567caa7504f301aeb51c493b7c4286d48
Joe Berkheiser <berkheij@mail.gvsu.edu>
2025-03-12 09:55:30 -0400

    testing vision api setup

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,28 @@
 from flask import Flask, jsonify
-import random
+import requests
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/get-problem")
-def getMathProblem():
-    operatorList = ['+', '-', '*', '/']
-    firstNumber = 3
-    secondNumber = 45
-    operator = random.choice(operatorList)
+@app.route("/get-image-description")
+def getImageDescription():
+    projectID = "cis655-vision-api-project"
+    jsonContent = ""
+    contentType = "application/json; charset=utf-8"
+    visionAPIURL = "https://vision.googleapis.com/v1/images:annotate"
+    requestHeaders = {"x-goog-user-project": projectID, "Content-Type": contentType}
 
-    retString = jsonify({"firstNumber": firstNumber, "secondNumber": secondNumber, "operator": operator})
-    return retString
+    with open("request.json", "r") as file:
+        jsonContent = file.read()
+
+    generatedDescription = requests.post(visionAPIURL, json=jsonContent, headers=requestHeaders)
+    # curl -X POST \
+    # -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    # -H "x-goog-user-project: PROJECT_ID" \
+    # -H "Content-Type: application/json; charset=utf-8" \
+    # https://vision.googleapis.com/v1/images:annotate -d @request.json
+    return generatedDescription
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
